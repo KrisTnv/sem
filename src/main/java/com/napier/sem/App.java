4ca0f84d70c0e372e91154450e4f6635ea512a95
KrisTnv <krisso.k@abv.bg>
2021-02-17 11:20:43 +0000

    Updated app

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -3,15 +3,18 @@ package com.napier.sem;
 import java.sql.*;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         // Create new Application
         App a = new App();
+
         // Connect to database
         a.connect();
         // Get Employee
         Employee emp = a.getEmployee(255530);
         // Display results
         a.displayEmployee(emp);
+
         // Disconnect from database
         a.disconnect();
     }
@@ -71,43 +74,34 @@ public class App {
         }
     }
 
-    public Employee getEmployee(int ID) {
-        try {
+    public Employee getEmployee(int ID)
+    {
+        try
+        {
             // Create an SQL statement
             Statement stmt = con.createStatement();
             // Create string for SQL statement
-
-            //could break this down into two SQL statements to retrieve employee details using joins
-            // then another query to get the manager
-
-            String strSelect = "SELECT e1.emp_no, e1.first_name, e1.last_name, titles.title, salaries.salary, " +
-                    "dp1.dept_name, e2.first_name as manager_firstname, e2.last_name as manager_lastname " +
-                    "FROM employees e1 JOIN titles ON titles.emp_no = e1.emp_no " +
-                    "JOIN dept_emp ON dept_emp.emp_no = e1.emp_no " +
-                    "JOIN departments dp1 ON dp1.dept_no = dept_emp.dept_no " +
-                    "JOIN dept_manager dm1 ON dm1.dept_no = dp1.dept_no " +
-                    "JOIN salaries ON salaries.emp_no = e1.emp_no JOIN employees e2 ON e2.emp_no IN " +
-                    "(SELECT dm2.emp_no FROM dept_manager dm2 WHERE dm2.dept_no = dp1.dept_no AND dm2.to_date = '9999-01-01') " +
-                    "WHERE dept_emp.emp_no = '" + ID + "' AND salaries.to_date = '9999-1-1' AND dm1.to_date = '9999-1-1' " +
-                    "AND titles.to_date = '9999-1-1' AND dept_emp.to_date = '9999-1-1';" ;
+            String strSelect =
+                    "SELECT emp_no, first_name, last_name "
+                            + "FROM employees "
+                            + "WHERE emp_no = " + ID;
             // Execute SQL statement
             ResultSet rset = stmt.executeQuery(strSelect);
             // Return new employee if valid.
             // Check one is returned
-            if (rset.next()) {
+            if (rset.next())
+            {
                 Employee emp = new Employee();
                 emp.emp_no = rset.getInt("emp_no");
                 emp.first_name = rset.getString("first_name");
                 emp.last_name = rset.getString("last_name");
-                emp.title = rset.getString("titles.title");
-                emp.salary = rset.getInt("salaries.salary");
-                emp.dept_name = rset.getString("dp1.dept_name");
-                emp.manager = rset.getString("manager_firstname") + " " + rset.getString("manager_lastname");
-                ;
                 return emp;
-            } else
+            }
+            else
                 return null;
-        } catch (Exception e) {
+        }
+        catch (Exception e)
+        {
             System.out.println(e.getMessage());
             System.out.println("Failed to get employee details");
             return null;
@@ -115,8 +109,10 @@ public class App {
     }
 
 
-    public void displayEmployee(Employee emp) {
-        if (emp != null) {
+    public void displayEmployee(Employee emp)
+    {
+        if (emp != null)
+        {
             System.out.println(
                     emp.emp_no + " "
                             + emp.first_name + " "
